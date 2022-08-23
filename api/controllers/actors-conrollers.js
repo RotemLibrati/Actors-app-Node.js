32ae73accfc2fdaaab498c66ae65218bd69bbc2d
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-23 08:31:23 +0300

    Router

--- api/controllers/actors-conrollers.js
+++ api/controllers/actors-conrollers.js
@@ -1,13 +1,62 @@
 const axios = require('axios');
+const client = require('../../redis/redis');
+
+
+async function cache(req, res) {
+    var keys;
+    await client.keys('*')
+        .then(result => {
+            keys = result;
+        })
+        .catch(err => {
+            console.log("error:", err.message);
+        });
+    return keys;
+}
 
 const getActors = async (req, res) => {
+    const data = [];
     try {
-        const actors = await axios.get('https://api.tvmaze.com/shows/1/cast');
-        res.json(actors.data);
-    } catch(err){
-        console.error(err);
+        const keys = await cache();
+        if (keys.length > 0) {
+            keys.map(key => {
+                client.get(key)
+                    .then((result) => {
+                        data.push(JSON.parse(result));    
+                    })
+                    .catch((err) => {
+                        console.error("error:", err.message);
+                    })
+            });
+            console.log("redis");
+            setTimeout(() => {
+                res.json(data);
+            }, 3000);
+
+        } else {
+            const actors = await axios.get('https://api.tvmaze.com/shows/1/cast');
+            actors.data.map(actor => {
+                client.setEx(JSON.stringify(actor.person.id), 30, JSON.stringify({
+                    "id": actor.person.id,
+                    "name": actor.person.name,
+                    "birthday": actor.person.birthday,
+                    "gender": actor.person.gender
+                }))
+            });
+            console.log("api");
+            actors.data.map(actor => {
+                data.push({
+                    "id": actor.person.id,
+                    "name": actor.person.name,
+                    "birthday": actor.person.birthday,
+                    "gender": actor.person.gender
+                })
+            })
+            res.json(data);
+        }
+    } catch (err) {
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 };
-
 exports.getActors = getActors;
--- api/routers/actors-router.js
+++ api/routers/actors-router.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 
 const { getActors } = require('../controllers/actors-conrollers');
