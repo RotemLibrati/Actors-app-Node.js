bae788d8ee2cf081af99c031437c087fd7b4fe11
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-23 08:32:10 +0300

    App server

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.json({ extended: false}));
 
 app.use('/api/actors', require('./api/routers/actors-router'));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
