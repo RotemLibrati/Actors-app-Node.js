274c305e11c1155dd826e10d3fef71ad669c15e5
RotemLibrati <rotemlibrati1@gmail.com>
2022-08-23 12:14:45 +0300

    Added http request - post and delete

--- api/routers/actors-router.js
+++ api/routers/actors-router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getActors } = require('../controllers/actors-conrollers');
+const { getActors, writeComment, deleteActor } = require('../controllers/actors-conrollers');
 
 router.get('/', getActors);
+router.post('/', writeComment);
+router.delete('/', deleteActor);
 
 module.exports = router;
